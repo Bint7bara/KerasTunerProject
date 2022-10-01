1343882c7a62c5ee23669b80447a664cb8dfe3f2
Morris Chukhman <chukhman@login-1.extreme.acer.uic.edu>
2022-10-01 06:47:38 -0500

    adjust number of epochs, model depth and max layer size and name

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ y = dataset[:,5]
 
 def build_model(hp):
     model = keras.Sequential()
-    for i in range(hp.Int('num_layers', 2, 6)):
+    for i in range(hp.Int('num_layers', 2, 4)):
         model.add(layers.Dense(units=hp.Int('units_' + str(i),
                                             min_value=20,
-                                            max_value=250,
+                                            max_value=200,
                                             step=10),
                                activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
@@ -36,7 +36,7 @@ tuner = RandomSearch(
     max_trials=20,
     executions_per_trial=3,
     directory='project',
-    project_name='Gencode')
+    project_name='Gencode2')
 
 tuner.search_space_summary()
 
@@ -44,7 +44,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 tuner.search(X_train, y_train,
-             epochs=250,
+             epochs=150,
              validation_data=(X_test, y_test))
 
 tuner.results_summary()
